21ea506a5b123df74ba54f19ecd0efdd9a8e0243
Rinjax <steve@encara.co.uk>
2019-07-08 21:02:52 +0100

    saving...

--- src/Resources/Views/components/header.blade.php
+++ src/Resources/Views/components/header.blade.php
@@ -1,10 +1,11 @@
 <header>
+    @include('website::components.menu')
     <div class="container-fluid">
-        @include('website::components.menu')
+
     </div>
     <div class="container">
 
-        <div style="width: max-content">
+        <div class="d-md-block" style="width: max-content">
             <h1>Rinjax Web Development</h1>
             <h5 class="text-right">a laravel developer</h5>
         </div>
--- src/Resources/Views/components/menu.blade.php
+++ src/Resources/Views/components/menu.blade.php
@@ -1,9 +1,9 @@
-<nav class="navbar navbar-expand-md sticky-top ml-auto">
-
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+<nav class="navbar navbar-expand-md sticky-top ml-auto main-nav">
+    <h3 class="d-md-none">Rinjax</h3>
+    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="icon-chevrons-down"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
         <div class="navbar-nav ml-auto">
             <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
             <a class="nav-item nav-link" href="#">Features</a>
