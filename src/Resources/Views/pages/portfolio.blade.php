50ce9083a06456541e258e2f03a960016d8df303
Rinjax <steve@encara.co.uk>
2019-08-20 21:10:01 +0100

    saving...

--- src/Resources/Views/pages/portfolio.blade.php
+++ src/Resources/Views/pages/portfolio.blade.php
@@ -3,4 +3,24 @@
 @section('content')
 
 
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="spinner spinner-1"></div>
+                        <div class="spinner spinner-2"></div>
+                        <div class="spinner spinner-3"></div>
+                        <div class="spinner spinner-4"></div>
+                        <div class="spinner spinner-5"></div>
+                        <div class="dotter dotter-1"><span></span><span></span><span></span></div>
+                        <div class="dotter dotter-2"><span></span><span></span><span></span></div>
+                        <div class="dotter dotter-3"><span></span><span></span><span></span></div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
